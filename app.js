a23aa0514a4bb76a8460a19f260807019b5a0c4a
JTVictor <jtownsend@outlook.com>
2018-05-18 15:55:56 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+console.log('Working');
+
+const grid = [
+  [1,0,0,0,0,0,1,0,0,0],
+  [0,0,0,0,0,0,1,0,0,0],
+  [0,0,0,0,1,1,1,1,1,0],
+  [0,0,0,0,0,0,1,0,0,0],
+  [0,0,0,1,0,0,1,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,1,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0]
+];
+
+$(()=>{
+
+  $('#map').on('mouseover', 'div', function(){
+    $('#cell-address').val(`${$(this).data('x')}-${$(this).data('y')}`)
+  });
+
+  $.each(grid, (i, row) => {
+    $.each(row, (j, cell) => {
+      const $element = $('<div />');
+      if(cell === 0) {
+        $element.addClass('blank');
+      } else if (cell === 1) {
+        $element.addClass('path');
+      }
+
+      $element.data({x: i, y: j});
+
+      $element.on('click'), function(){
+        console.log($(this));
+      };
+      $element.appendTo('#map');
+    });
+  });
+});
